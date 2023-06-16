69beb862d03ed843a6fc074dbdee593a461fa892
Mathis Méheux <mat.mhx@laposte.net>
2023-06-17 00:33:23 +0200

    Etape 4 bientôt terminée

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 
 mongoose.connect('mongodb+srv://mathis-piiquante:piiquante@cluster1.0dyqvkc.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -11,6 +13,8 @@ mongoose.connect('mongodb+srv://mathis-piiquante:piiquante@cluster1.0dyqvkc.mong
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -18,8 +22,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
-
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
