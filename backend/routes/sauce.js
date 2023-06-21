4051896b95b9bbc68e85ff8fe80f46231afb196a
Mathis Méheux <mat.mhx@laposte.net>
2023-06-21 19:54:31 +0200

    Code du fichier commenté.

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -6,6 +6,8 @@ const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauce');
 
+// Les routes définies avec les différentes méthodes HTTP (POST, PUT, DELETE, GET) ne peuvent être accessibles en premier lieu
+// que si le jeton vérifié est valide (donc, uniquement si l'utilisateur à bien un compte valide authentifié)
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
